9eaead5cb5f29387fb2b5893c2aa830b8343123c
t4yl4n <taylangerdan@gmail.com>
2024-11-25 20:11:17 -0500

    initial commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Versions from './components/Versions.vue'
+
+const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <img alt="logo" class="logo" src="./assets/electron.svg" />
+  <Versions />
+</template>
